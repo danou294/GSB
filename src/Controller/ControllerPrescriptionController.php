2d9e1aeecf09f8a87e10697ed53e4f11d27e810d
Tony891 <72949320+Tony891@users.noreply.github.com>
2021-02-11 23:16:41 +0100

    Ajout de la page Prescription et des fichiers css

---
+++ src/Controller/ControllerPrescriptionController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ControllerPrescriptionController extends AbstractController
+{
+    /**
+     * @Route("/controller/prescription", name="controller_prescription")
+     */
+    public function index(): Response
+    {
+        return $this->render('pages/Prescription.html.twig', [
+            'controller_name' => 'ControllerPrescriptionController',
+        ]);
+    }
+}
