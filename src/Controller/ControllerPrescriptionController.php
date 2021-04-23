26235be0fb90b16e857fdfa974e90d818673591c
Tony891 <72949320+Tony891@users.noreply.github.com>
2021-04-23 02:20:44 +0200

    Changement prescription

--- src/Controller/ControllerPrescriptionController.php
+++ src/Controller/ControllerPrescriptionController.php
@@ -2,20 +2,30 @@
 
 namespace App\Controller;
 
+use App\Entity\Prescrire;
+use App\Entity\TypeIndividu;
+use Doctrine\ORM\EntityManagerInterface;
+use http\Env\Request;
+use phpDocumentor\Reflection\Types\Integer;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ControllerPrescriptionController extends AbstractController
 {
+
+
+
     /**
-     * @Route("/controller/prescription", name="controller_prescription")
+     * @Route("/controller/prescription", name="prescription")
      */
 
-    public function index (): Response
+    public function Formulaire ()
     {
         return $this->render('pages/Prescription.html.twig', [
             'controller_name' => 'ControllerPrescriptionController'
         ]);
     }
+
 }
